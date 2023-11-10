003e82a56b4eaecf5d3649cc15364c6f55285798
ReeceHahn <69384775+ReeceHahn@users.noreply.github.com>
2023-11-10 03:31:35 +0000

    encoders.py file

--- encoders.py
+++ encoders.py
@@ -6,16 +6,12 @@ import time
 encoder1 = pulseio.PulseIn(board.GP12, maxlen=5000)
 encoder2 = pulseio.PulseIn(board.GP20, maxlen=5000)
 
-#Initialise count variables
-encoder1_count = 0
-encoder2_count = 0
-
 #Initialise previous time variables
 encoder1_previous_time = time.monotonic()
 encoder2_previous_time = time.monotonic()
 
 #Define encoder pulses per revolution
-pulses_per_revolution = 150
+pulses_per_revolution = 300
 
 # Function to calculate RPS from encoder counts
 def calculate_rps(count, time_interval):
@@ -27,57 +23,51 @@ def calculate_rps(count, time_interval):
         return 0
 
 def left_rps():
-    global encoder1, encoder1_count, encoder1_previous_time
+    global encoder1_previous_time
     
+    #Count the pulses
+    encoder1_count = len(encoder1)
+    
+    #Calculate the time interval between the previous count
     current_time = time.monotonic()
     time_interval = current_time - encoder1_previous_time
 
-    # Calculate RPS
+    #Calculate RPS
     rps = calculate_rps(encoder1_count, time_interval)
 
-    # Reset count
-    encoder1_count = 0
-
-    # Update the previous time
-    encoder1_previous_time = current_time
-
-    # Reset the pulsein object to start counting again
+    #Pause and clear the pulse counter
     encoder1.pause()
     encoder1.clear()
+    
+    #Reset the previous time
+    encoder1_previous_time = current_time
+    
+    #Resume the pulse counter
     encoder1.resume()
-
-    # Sleep for a short duration to control the update rate
-    time.sleep(0.1)
-
-    # Count the pulses to update counts
-    encoder1_count = len(encoder1)
     
     return rps
 
 def right_rps():
-    global encoder2, encoder2_count, encoder2_previous_time
+    global encoder2_previous_time
     
+    #Count the pulses
+    encoder2_count = len(encoder2)
+    
+    #Calculate the time interval between the previous count
     current_time = time.monotonic()
     time_interval = current_time - encoder2_previous_time
 
-    # Calculate RPS
+    #Calculate RPS
     rps = calculate_rps(encoder2_count, time_interval)
 
-    # Reset count
-    encoder2_count = 0
-
-    # Update the previous time
-    encoder2_previous_time = current_time
-
-    # Reset the pulsein object to start counting again
+    #Pause and clear the pulse counter
     encoder2.pause()
     encoder2.clear()
+    
+    #Reset the previous time
+    encoder2_previous_time = current_time
+    
+    #Resume the pulse counter
     encoder2.resume()
-
-    # Sleep for a short duration to control the update rate
-    time.sleep(0.1)
-
-    # Count the pulses to update counts
-    encoder2_count = len(encoder2)
     
     return rps
