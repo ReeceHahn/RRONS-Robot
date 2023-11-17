f0c517a9b43fe5f8b543c9b7f7a5f5d276e6e271
ReeceHahn <69384775+ReeceHahn@users.noreply.github.com>
2023-11-17 10:35:54 +0000

    Update encoders.py

--- encoders.py
+++ encoders.py
@@ -3,17 +3,16 @@ import pulseio
 import time
 
 #Define pulsein objects to count encoder A pin pulses (B is ignored since we are not concerned about direction)
-encoder1 = pulseio.PulseIn(board.GP12, maxlen=5000)
-encoder2 = pulseio.PulseIn(board.GP20, maxlen=5000)
+encoder1 = pulseio.PulseIn(board.GP12, maxlen=10000)
+encoder2 = pulseio.PulseIn(board.GP20, maxlen=10000)
 
 #Initialise previous time variables
 encoder1_previous_time = time.monotonic()
 encoder2_previous_time = time.monotonic()
 
 #Define encoder pulses per revolution
-pulses_per_revolution = 300
+pulses_per_revolution = 300 #This value was determined experimentally, I can't find the true value anywhere on the encoder website as there is no datasheet available to my knowledge
 
-# Function to calculate RPS from encoder counts
 def calculate_rps(count, time_interval):
     if time_interval != 0:
         revolutions = count / pulses_per_revolution
@@ -25,6 +24,9 @@ def calculate_rps(count, time_interval):
 def left_rps():
     global encoder1_previous_time
     
+    #Pause pulse counter
+    encoder1.pause()
+    
     #Count the pulses
     encoder1_count = len(encoder1)
     
@@ -35,8 +37,7 @@ def left_rps():
     #Calculate RPS
     rps = calculate_rps(encoder1_count, time_interval)
 
-    #Pause and clear the pulse counter
-    encoder1.pause()
+    #Clear the pulse counter
     encoder1.clear()
     
     #Reset the previous time
@@ -50,6 +51,9 @@ def left_rps():
 def right_rps():
     global encoder2_previous_time
     
+    #Pause pulse counter
+    encoder2.pause()
+    
     #Count the pulses
     encoder2_count = len(encoder2)
     
@@ -60,8 +64,7 @@ def right_rps():
     #Calculate RPS
     rps = calculate_rps(encoder2_count, time_interval)
 
-    #Pause and clear the pulse counter
-    encoder2.pause()
+    #Clear the pulse counter
     encoder2.clear()
     
     #Reset the previous time
